61872ab34bced8a0385b241860874a90adbff2f5
Cheng-Han <cheng-han.wu@cern.ch>
2022-06-08 13:56:43 +0800

    update UL full run2 configs

---
+++ UL2018.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --checkBins
+# python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --createBins
+# python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --createHists
+python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --doFit
+
+python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --doFit --mcSig --altSig
+python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --doFit --altSig
+python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --doFit --altBkg
+
+python tnpEGM_fitter.py etc/config/settings_ele_UL2018_HDalitz_misshits.py --flag passingMissHits --sumUp
